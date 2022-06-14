ebea57ec97d9d30921d717e7a9351ca1ff73073d
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-06-14 12:56:30 +0100

    updated testing related config files

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
+import presets from 'ts-jest/presets/index.js';
+const { jsWithBabel: tsjPreset } = presets;
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
 	transform: {
 		'^.+\\.svelte$': ['svelte-jester', { preprocess: true }],
-		'^.+\\.ts$': 'ts-jest'
+		...tsjPreset.transform
 	},
-	moduleFileExtensions: ['js', 'ts', 'svelte'],
+	moduleFileExtensions: ['ts', 'svelte', 'js'],
 	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
+	testEnvironmentOptions: { pretendToBeVisual: true },
+	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+	setupFiles: ['jest-canvas-mock'],
+	moduleNameMapper: {
+		'^\\$lib$': '<rootDir>/src/lib',
+		'^\\$lib/(.*)$': '<rootDir>/src/lib/$1',
+		'^\\$app/(.*)$': '<rootDir>/.svelte-kit/runtime/app/$1'
+	}
 };
