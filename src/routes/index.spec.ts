c9129009d9ad8d94aa4e777bb4eda52912d931e0
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-06-18 07:00:21 +0100

    test: Update index.spec.ts
    Changed test to show the popup when device orientation is portrait
    instead of landscape. Also removed repetitive and unnecessary tests.

--- src/routes/index.spec.ts
+++ src/routes/index.spec.ts
@@ -19,30 +19,11 @@ describe('Renders content', () => {
 		expect(container).toBeInTheDocument();
 	});
 
-	it('renders the canvas', () => {
-		const { getByTitle } = render(Page);
-		expect(getByTitle('canvas')).toBeInTheDocument();
-	});
-
-	it("doesn't show dialog if a desktop device is in landscape", async () => {
-		const { queryByText } = render(Page);
-		expect(queryByText('Flip your screen')).not.toBeInTheDocument();
-	});
-
-	it("doesn't show dialog if a mobile device is in portrait", async () => {
+	it('shows popup if a mobile device is in portrait', async () => {
 		const { queryByText } = render(Page);
 		window.innerWidth = 412;
 		window.innerHeight = 914;
 		matchMedia.useMediaQuery('(orientation: portrait)');
-		await fireEvent.resize(window);
-		expect(queryByText('Flip your screen')).not.toBeInTheDocument();
-	});
-
-	it('shows dialog if a mobile device is in landscape', async () => {
-		const { queryByText } = render(Page);
-		window.innerHeight = 412;
-		window.innerWidth = 914;
-		matchMedia.useMediaQuery('(orientation: landscape)');
 		await fireEvent.resize(window);
 		expect(queryByText('Flip your screen')).toBeInTheDocument();
 	});
