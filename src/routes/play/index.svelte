35777275e1e6a985e3be529f3a8655f390a8ee23
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-08 17:25:00 +0100

    refactor: move game component to dedicated folder

---
+++ src/routes/play/index.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Canvas from '$lib/canvas.svelte';
+	import Snake from '$lib/snake/Snake.svelte';
+</script>
+
+<section class="flex flex-col items-center justify-center gap-5">
+	<Canvas>
+		<Snake />
+	</Canvas>
+	<div class="text-2xl">0</div>
+</section>
