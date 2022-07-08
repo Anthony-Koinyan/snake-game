3968648fab0ae5f746cf271105b2cc504f654af5
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-08 18:10:08 +0100

    test: update to wait for mount functions to finish

--- src/routes/play/tests/play.spec.ts
+++ src/routes/play/tests/play.spec.ts
@@ -1,14 +1,14 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/svelte';
+import { act, render, screen } from '@testing-library/svelte';
 import Play from '../../play/index.svelte';
 
 // let food: Food;
 describe('checking for default content on initial render', () => {
-	it('has the snake and the scoreboard', () => {
+	it('has the snake and the scoreboard', async () => {
 		render(Play);
 		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
 		const ctx = canvas.getContext('2d');
-
+		await act();
 		expect(ctx?.fillRect).toBeCalledTimes(1);
 		expect(screen.getByText(0)).toBeInTheDocument();
 	});
