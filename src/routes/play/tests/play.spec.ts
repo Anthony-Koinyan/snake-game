018467da3a811bd1fd3af0d8d6bf25c9bdbd2ce6
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-09 01:59:56 +0100

    test: render component with props

--- src/routes/play/tests/play.spec.ts
+++ src/routes/play/tests/play.spec.ts
@@ -5,7 +5,13 @@ import Play from '../../play/index.svelte';
 // let food: Food;
 describe('checking for default content on initial render', () => {
 	it('has the snake and the scoreboard', async () => {
-		render(Play);
+		render(Play, {
+			props: {
+				canvasWidth: window.innerWidth,
+				canvasHeight: window.innerHeight
+			}
+		});
+
 		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
 		const ctx = canvas.getContext('2d');
 		await act();
