c91aea08bbcc0375c478ce82a18085003472c0a4
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-16 23:48:56 +0100

    test: Update test

--- src/routes/play/tests/play.spec.ts
+++ src/routes/play/tests/play.spec.ts
@@ -10,6 +10,9 @@ describe('checking for default content on initial render', () => {
 		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
 		const ctx = canvas.getContext('2d');
 		await act();
+		await act();
+		await act();
+		await act();
 		expect(ctx?.fillRect).toBeCalledTimes(1);
 		expect(screen.getByText(0)).toBeInTheDocument();
 	});
