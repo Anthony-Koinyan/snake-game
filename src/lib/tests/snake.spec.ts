de3621725ca3bfe5ba6d105704d6c37e38405ff9
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-18 02:38:46 +0100

    test: Test if snake moves properly

--- src/lib/tests/snake.spec.ts
+++ src/lib/tests/snake.spec.ts
@@ -1,12 +1,15 @@
 // TODO: move this to test folder
 
 import { render, screen } from '@testing-library/svelte';
+import { get } from 'svelte/store';
 import Snake from '../snake';
 import type { SnakePosition } from '../snake';
+import { SNAKE_SPEED } from '../snake/store';
 import Canvas from '../canvas/Canvas.svelte';
+import { GAME_PIECE_MIN_SIZE } from '../stores';
 
 describe('Instatiates the snake class properly', () => {
-	const coords: SnakePosition = {
+	const position: SnakePosition = {
 		x1: 40,
 		x2: 190,
 		y1: 300,
@@ -26,30 +29,34 @@ describe('Instatiates the snake class properly', () => {
 		]
 	};
 
-	it('can be instantiated with a coords object', () => {
-		const snake = new Snake(coords, 1, 15);
+	it('can be instantiated with a position object', () => {
+		const snake = new Snake(position, 1, 15);
 		expect(snake).toMatchObject(match);
 	});
 
-	it('can be instantiated with an array of coords ', () => {
-		const snake = new Snake([coords], 1, 15);
+	it('can be instantiated with an array of position ', () => {
+		const snake = new Snake([position], 1, 15);
 		expect(snake).toMatchObject(match);
 	});
 });
 
 describe('renders snake and clears snake properly', () => {
 	let ctx: CanvasRenderingContext2D;
-	let coords: SnakePosition[];
+	let position: SnakePosition[];
 	let snake: Snake;
 
 	beforeEach(() => {
-		render(Canvas);
+		render(Canvas, {
+			props: {
+				container: document.createElement('section')
+			}
+		});
 		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
 		ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 	});
 
 	it('draws a rectangle for all positions in the body', () => {
-		coords = [
+		position = [
 			{
 				x1: 40,
 				x2: 190,
@@ -59,19 +66,19 @@ describe('renders snake and clears snake properly', () => {
 			}
 		];
 
-		snake = new Snake(coords, 1, 15);
+		snake = new Snake(position, 1, 15);
 		snake.draw(ctx);
 		expect(ctx.fillRect).toBeCalledTimes(1);
 		expect(ctx.fillRect).toBeCalledWith(
-			coords[0].x1,
-			coords[0].y1,
-			coords[0].x2 - coords[0].x1,
-			coords[0].y2 - coords[0].y1
+			position[0].x1,
+			position[0].y1,
+			position[0].x2 - position[0].x1,
+			position[0].y2 - position[0].y1
 		);
 	});
 
-	it("draws rectangle for all coords when snake body's length greater than 1", () => {
-		coords = [
+	it("draws rectangle for all position when snake body's length greater than 1", () => {
+		position = [
 			{
 				x1: 40,
 				x2: 190,
@@ -88,13 +95,13 @@ describe('renders snake and clears snake properly', () => {
 			}
 		];
 
-		snake = new Snake(coords, 1, 15);
+		snake = new Snake(position, 1, 15);
 		snake.draw(ctx);
 		expect(ctx.fillRect).toBeCalledTimes(2);
 	});
 
 	it('only clears snake if snake has been drawn', () => {
-		coords = [
+		position = [
 			{
 				x1: 40,
 				x2: 190,
@@ -104,7 +111,7 @@ describe('renders snake and clears snake properly', () => {
 			}
 		];
 
-		const snake = new Snake(coords, 1, 15);
+		const snake = new Snake(position, 1, 15);
 		snake.clear(ctx);
 		expect(ctx.clearRect).not.toBeCalled();
 
@@ -113,15 +120,15 @@ describe('renders snake and clears snake properly', () => {
 
 		expect(ctx.clearRect).toBeCalled();
 		expect(ctx.clearRect).toBeCalledWith(
-			coords[0].x1,
-			coords[0].y1,
-			coords[0].x2 - coords[0].x1,
-			coords[0].y2 - coords[0].y1
+			position[0].x1 - 2,
+			position[0].y1 - 2,
+			position[0].x2 - position[0].x1 + 4,
+			position[0].y2 - position[0].y1 + 4
 		);
 	});
 
-	it("clears rectangle for all coords when snake body's length greater than 1", () => {
-		coords = [
+	it("clears rectangle for all position when snake body's length greater than 1", () => {
+		position = [
 			{
 				x1: 40,
 				x2: 190,
@@ -138,9 +145,169 @@ describe('renders snake and clears snake properly', () => {
 			}
 		];
 
-		const snake = new Snake(coords, 1, 15);
+		const snake = new Snake(position, 1, 15);
 		snake.draw(ctx);
 		snake.clear(ctx);
 		expect(ctx.clearRect).toBeCalledTimes(2);
+	});
+});
+
+describe('the snake can move', () => {
+	let ctx: CanvasRenderingContext2D;
+	let speed: number;
+	let thickness: number;
+	let position: SnakePosition[];
+
+	beforeAll(() => {
+		speed = get(SNAKE_SPEED);
+		thickness = get(GAME_PIECE_MIN_SIZE);
+	});
+
+	beforeEach(() => {
+		render(Canvas, {
+			props: {
+				container: document.createElement('section')
+			}
+		});
+		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
+		ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+	});
+
+	it("doesn't move the snake if it has not been drawn", () => {
+		position = [
+			{
+				x1: 40,
+				x2: 190,
+				y1: 300,
+				y2: 300 + thickness,
+				direction: 'right'
+			},
+			{
+				x1: 40,
+				x2: 190,
+				y1: 200,
+				y2: 315,
+				direction: 'up'
+			}
+		];
+
+		const snake = new Snake(position, speed, thickness);
+		snake.move();
+		expect(snake.head).toEqual(position[0]);
+		expect(snake.tail).toEqual(position[1]);
+	});
+
+	it('moves the snake right if the head and tail directions are right', () => {
+		position = [
+			{
+				x1: 40,
+				x2: 190,
+				y1: 300,
+				y2: 300 + thickness,
+				direction: 'right'
+			}
+		];
+
+		const snake = new Snake(position, speed, thickness);
+		snake.draw(ctx);
+		snake.move();
+		expect(snake.head.x1).toBe(position[0].x1 + speed);
+		expect(snake.tail.x2).toBe(position[0].x2 + speed);
+		expect(snake.head.y1).toBe(position[0].y1);
+		expect(snake.head.y2).toBe(position[0].y2);
+	});
+
+	it('moves the snake left if the head and tail directions are left', () => {
+		position = [
+			{
+				x1: 40,
+				x2: 190,
+				y1: 300,
+				y2: 300 + thickness,
+				direction: 'left'
+			}
+		];
+
+		const snake = new Snake(position, speed, thickness);
+		snake.draw(ctx);
+		snake.move();
+
+		expect(snake.head.x1).toBe(position[0].x1 - speed);
+		expect(snake.head.x2).toBe(position[0].x2 - speed);
+		expect(snake.head.y1).toBe(position[0].y1);
+		expect(snake.head.y2).toBe(position[0].y2);
+	});
+
+	it('moves the snake up if the head and tail directions are up', () => {
+		position = [
+			{
+				x1: 40,
+				x2: 40 + thickness,
+				y1: 40,
+				y2: 190,
+				direction: 'up'
+			}
+		];
+
+		const snake = new Snake(position, speed, thickness);
+		snake.draw(ctx);
+		snake.move();
+
+		expect(snake.head.x1).toBe(position[0].x1);
+		expect(snake.head.x2).toBe(position[0].x2);
+		expect(snake.head.y1).toBe(position[0].y1 - speed);
+		expect(snake.head.y2).toBe(position[0].y2 - speed);
+	});
+
+	it('moves the snake down if the head and tail directions are down', () => {
+		position = [
+			{
+				x1: 40,
+				x2: 40 + thickness,
+				y1: 40,
+				y2: 190,
+				direction: 'down'
+			}
+		];
+
+		const snake = new Snake(position, speed, thickness);
+		snake.draw(ctx);
+		snake.move();
+
+		expect(snake.head.x1).toBe(position[0].x1);
+		expect(snake.head.x2).toBe(position[0].x2);
+		expect(snake.head.y1).toBe(position[0].y1 + speed);
+		expect(snake.head.y2).toBe(position[0].y2 + speed);
+	});
+
+	it('moves snake head right and tail up if snake head direction is right and tail direction is up', () => {
+		position = [
+			{
+				x1: 40,
+				x2: 190,
+				y1: 300,
+				y2: 300 + thickness,
+				direction: 'right'
+			},
+			{
+				x1: 40,
+				x2: 40 + thickness,
+				y1: 300,
+				y2: 315,
+				direction: 'up'
+			}
+		];
+		const snake = new Snake(position, speed, thickness);
+		snake.draw(ctx);
+		snake.move();
+		expect(snake.head.x1).toBe(position[0].x1);
+		expect(snake.head.x2).toBe(position[0].x2 + speed);
+		expect(snake.head.y1).toBe(position[0].y1);
+		expect(snake.head.y2).toBe(position[0].y2);
+
+		expect(snake.tail.x1).toBe(position[position.length - 1].x1);
+		expect(snake.tail.x2).toBe(position[position.length - 1].x2);
+		expect(snake.tail.y1).toBe(position[position.length - 1].y1);
+		expect(snake.tail.y2).toBe(position[position.length - 1].y2 - speed);
 	});
 });
