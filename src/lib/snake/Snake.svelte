9bc7cc9307eaa3b2267c57ee09ae550e85e154d7
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-18 02:38:16 +0100

    refactor: Use the new canvas rendering api to draw/animate the snake

--- src/lib/snake/Snake.svelte
+++ src/lib/snake/Snake.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
-	import { ctxKey, SNAKE_SPEED, SNAKE_START_POSITION, GAME_PIECE_MIN_SIZE } from '$lib/settings';
-	import { getContext, onMount, tick } from 'svelte';
+	import { getContext } from 'svelte';
+	import { GAME_PIECE_MIN_SIZE, RENDER_CONTEXT_KEY } from '$lib/stores';
+	import { SNAKE_SPEED, SNAKE_START_POSITION } from './store';
 	import Snake from './index';
+	import type { RenderContext } from '../canvas/types';
 
+	const { addRenderFn } = getContext<RenderContext>(RENDER_CONTEXT_KEY);
 	const snake = new Snake($SNAKE_START_POSITION, $SNAKE_SPEED, $GAME_PIECE_MIN_SIZE);
-	const { getCanvasContext } = getContext(ctxKey);
 
-	onMount(async () => {
-		const ctx: CanvasRenderingContext2D = getCanvasContext();
-		await tick();
-
-		snake.draw(ctx);
+	addRenderFn({
+		renderFn: (ctx: CanvasRenderingContext2D) => {
+			snake.clear(ctx);
+			snake.draw(ctx);
+			snake.move();
+		},
+		animate: true
 	});
 </script>
