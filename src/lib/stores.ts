412784886cd81d1be8d0b70bd47ea3aa56a7edd3
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-19 03:14:18 +0100

    refactor: Change GAME_PIECE_MIN_SIZE to 6

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,5 @@
 import { readable } from 'svelte/store';
 
-export const GAME_PIECE_MIN_SIZE = readable(20);
+export const GAME_PIECE_MIN_SIZE = readable(6);
 // TODO: make this a store (I think, not sure🤷🏾‍♂️)
 export const RENDER_CONTEXT_KEY = Symbol();
