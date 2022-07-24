dc8a397c08f53f4da90e919f66ad102b6e81eded
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-24 15:37:32 +0100

    test: Check if food is drawn on load
    This tests that food is drawn when the game is loaded

--- src/routes/play/tests/play.spec.ts
+++ src/routes/play/tests/play.spec.ts
@@ -1,17 +1,34 @@
 import '@testing-library/jest-dom';
 import { act, render, screen } from '@testing-library/svelte';
 import Play from '../../play/index.svelte';
+import { SNAKE_START_POSITION } from '$lib/snake/store';
+import { FOOD_POSITION } from '$lib/food/store';
+import { GAME_PIECE_MIN_SIZE } from '$lib/stores';
+import { get } from 'svelte/store';
 
-// let food: Food;
 describe('checking for default content on initial render', () => {
 	it('has the snake and the scoreboard', async () => {
+		const snakePosition = get(SNAKE_START_POSITION);
+		const foodPosition = get(FOOD_POSITION);
+		const foodRadius = get(GAME_PIECE_MIN_SIZE);
 		render(Play);
 
 		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
 		const ctx = canvas.getContext('2d');
+
 		await act();
 		await act();
-		expect(ctx?.fillRect).toBeCalledTimes(1);
+
+		expect(ctx?.fillRect).toBeCalledWith(
+			snakePosition.x1,
+			snakePosition.y1,
+			snakePosition.x2 - snakePosition.x1,
+			snakePosition.y2 - snakePosition.y1
+		);
+
+		expect(ctx?.arc).toBeCalledWith(foodPosition.x, foodPosition.y, foodRadius, 0, 2 * Math.PI);
+		expect(ctx?.fill).toBeCalledTimes(1);
+
 		expect(screen.getByText(0)).toBeInTheDocument();
 	});
 });
