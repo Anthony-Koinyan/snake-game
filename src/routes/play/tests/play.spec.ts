4386f5438997e0cd81c5aee2855e346784d3285e
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-07-25 10:15:19 +0100

    test: Separate 'has the snake and the scoreboard' test into multiple tests

--- src/routes/play/tests/play.spec.ts
+++ src/routes/play/tests/play.spec.ts
@@ -7,28 +7,34 @@ import { GAME_PIECE_MIN_SIZE } from '$lib/stores';
 import { get } from 'svelte/store';
 
 describe('checking for default content on initial render', () => {
-	it('has the snake and the scoreboard', async () => {
-		const snakePosition = get(SNAKE_START_POSITION);
-		const foodPosition = get(FOOD_POSITION);
-		const foodRadius = get(GAME_PIECE_MIN_SIZE) / 2;
+	const snakePosition = get(SNAKE_START_POSITION);
+	const foodPosition = get(FOOD_POSITION);
+	const foodRadius = get(GAME_PIECE_MIN_SIZE) / 2;
+	let ctx: CanvasRenderingContext2D | null;
+
+	beforeEach(async () => {
 		render(Play);
-
 		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
-		const ctx = canvas.getContext('2d');
-
+		ctx = canvas.getContext('2d');
 		await act();
 		await act();
+	});
 
+	it('draws the snake', () => {
 		expect(ctx?.fillRect).toBeCalledWith(
 			snakePosition.x1,
 			snakePosition.y1,
 			snakePosition.x2 - snakePosition.x1,
 			snakePosition.y2 - snakePosition.y1
 		);
+	});
 
+	it('draws food', () => {
 		expect(ctx?.arc).toBeCalledWith(foodPosition.x, foodPosition.y, foodRadius, 0, 2 * Math.PI);
 		expect(ctx?.fill).toBeCalledTimes(1);
+	});
 
+	it('has the scoreboard', () => {
 		expect(screen.getByText(0)).toBeInTheDocument();
 	});
 });
