99163d789a3c0d5130a3477127e81942ab0e812b
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-08-13 17:33:18 +0100

    refactor: move general setups outside test suites

--- src/routes/play/tests/play.spec.ts
+++ src/routes/play/tests/play.spec.ts
@@ -8,9 +8,27 @@ import type { SnakePosition } from '$lib/snake';
 import { FOOD_POSITION } from '$lib/food/store';
 import type { FoodPosition } from '$lib/food/types';
 
+const getSnakeCurrentPosition = () => get(SNAKE_POSITION);
+const getFoodCurrentPosition = () => get(FOOD_POSITION);
 const advanceTimersByTime = async (time: number) => {
 	await act(() => jest.advanceTimersByTime(time));
 };
+
+beforeAll(() => {
+	jest.useFakeTimers();
+	jest.spyOn(window, 'requestAnimationFrame');
+});
+
+afterAll(() => {
+	jest.runOnlyPendingTimers();
+	jest.useRealTimers();
+});
+
+beforeEach(async () => {
+	render(Play);
+	await act();
+	await act();
+});
 
 describe('checking for default content on initial render', () => {
 	const [snakePosition] = get(SNAKE_POSITION);
@@ -19,11 +37,8 @@ describe('checking for default content on initial render', () => {
 	let ctx: CanvasRenderingContext2D | null;
 
 	beforeEach(async () => {
-		render(Play);
 		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
 		ctx = canvas.getContext('2d');
-		await act();
-		await act();
 	});
 
 	it('draws the snake', () => {
@@ -45,121 +60,102 @@ describe('checking for default content on initial render', () => {
 	});
 });
 
-describe('tests for gameplay elements', () => {
+describe('snake can move', () => {
+	it('moves the snake', async () => {
+		const snakePosition = getSnakeCurrentPosition();
+		await advanceTimersByTime(16);
+		expect(getSnakeCurrentPosition()).not.toEqual(snakePosition);
+	});
+
+	it('can change snake direction with "WASD" keys', async () => {
+		expect(getSnakeCurrentPosition().length).toBe(1);
+		expect(getSnakeCurrentPosition()[0].direction).toBe('right');
+
+		fireEvent.keyPress(window, { key: 'W', code: 'KeyW' });
+		await advanceTimersByTime(100);
+		expect(getSnakeCurrentPosition().length).toBe(2);
+		expect(getSnakeCurrentPosition()[0].direction).toBe('up');
+
+		fireEvent.keyPress(window, { key: 'D', code: 'KeyD' });
+		await advanceTimersByTime(100);
+		expect(getSnakeCurrentPosition().length).toBe(3);
+		expect(getSnakeCurrentPosition()[0].direction).toBe('right');
+
+		fireEvent.keyPress(window, { key: 'S', code: 'KeyS' });
+		await advanceTimersByTime(100);
+		expect(getSnakeCurrentPosition().length).toBe(4);
+		expect(getSnakeCurrentPosition()[0].direction).toBe('down');
+
+		fireEvent.keyPress(window, { key: 'A', code: 'KeyA' });
+		await advanceTimersByTime(100);
+		expect(getSnakeCurrentPosition().length).toBe(5);
+		expect(getSnakeCurrentPosition()[0].direction).toBe('left');
+	});
+});
+
+describe('game updates properly when snake eats food', () => {
+	let scoreboard: HTMLElement;
+	const level = get(LEVEL);
+	let previousFoodPosition: FoodPosition;
+	let previousSnakePosition: SnakePosition[];
+
 	beforeEach(async () => {
-		jest.useFakeTimers();
-		jest.spyOn(window, 'requestAnimationFrame');
+		FOOD_POSITION.update(() => {
+			return { x: 175, y: 99 };
+		});
 
-		render(Play);
-		await act();
-		await act();
+		previousFoodPosition = getFoodCurrentPosition();
+		previousSnakePosition = getSnakeCurrentPosition();
+
+		scoreboard = screen.getByText(0);
+		await advanceTimersByTime(64);
 	});
 
-	const getSnakeCurrentPosition = () => get(SNAKE_POSITION);
-	const getFoodCurrentPosition = () => get(FOOD_POSITION);
-
-	describe('snake can move', () => {
-		it('moves the snake', async () => {
-			const snakePosition = getSnakeCurrentPosition();
-			await advanceTimersByTime(16);
-			expect(getSnakeCurrentPosition()).not.toEqual(snakePosition);
-		});
-
-		it('can change snake direction with "WASD" keys', async () => {
-			expect(getSnakeCurrentPosition().length).toBe(1);
-			expect(getSnakeCurrentPosition()[0].direction).toBe('right');
-
-			fireEvent.keyPress(window, { key: 'W', code: 'KeyW' });
-			await advanceTimersByTime(100);
-			expect(getSnakeCurrentPosition().length).toBe(2);
-			expect(getSnakeCurrentPosition()[0].direction).toBe('up');
-
-			fireEvent.keyPress(window, { key: 'D', code: 'KeyD' });
-			await advanceTimersByTime(100);
-			expect(getSnakeCurrentPosition().length).toBe(3);
-			expect(getSnakeCurrentPosition()[0].direction).toBe('right');
-
-			fireEvent.keyPress(window, { key: 'S', code: 'KeyS' });
-			await advanceTimersByTime(100);
-			expect(getSnakeCurrentPosition().length).toBe(4);
-			expect(getSnakeCurrentPosition()[0].direction).toBe('down');
-
-			fireEvent.keyPress(window, { key: 'A', code: 'KeyA' });
-			await advanceTimersByTime(100);
-			expect(getSnakeCurrentPosition().length).toBe(5);
-			expect(getSnakeCurrentPosition()[0].direction).toBe('left');
-		});
+	it('changes food position', () => {
+		expect(getFoodCurrentPosition()).not.toEqual(previousFoodPosition);
 	});
 
-	describe('game updates properly when snake eats food', () => {
-		let scoreboard: HTMLElement;
-		const level = get(LEVEL);
-		let previousFoodPosition: FoodPosition;
-		let previousSnakePosition: SnakePosition[];
+	it('draws food with its new position and clears the previous food', () => {
+		const ctx = (screen.getByTestId('canvas') as HTMLCanvasElement).getContext('2d');
+		const foodPosition = getFoodCurrentPosition();
+		const radius = get(GAME_PIECE_MIN_SIZE) / 2;
 
-		beforeEach(async () => {
-			scoreboard = screen.getByText(0);
+		expect(ctx?.arc).toBeCalledWith(foodPosition.x, foodPosition.y, radius, 0, 2 * Math.PI);
+		expect(ctx?.fill).toBeCalledTimes(1);
 
-			FOOD_POSITION.update(() => {
-				return { x: 175, y: 99 };
-			});
+		expect(ctx?.clearRect).toBeCalledWith(
+			previousFoodPosition.x - radius,
+			previousFoodPosition.y - radius,
+			radius * 2,
+			radius * 2
+		);
+	});
 
-			previousFoodPosition = getFoodCurrentPosition();
-			previousSnakePosition = getSnakeCurrentPosition();
-			await advanceTimersByTime(64);
-		});
+	it('increases the snake length', () => {
+		const snakePosition = getSnakeCurrentPosition();
 
-		it('changes food position', () => {
-			expect(getFoodCurrentPosition()).not.toEqual(previousFoodPosition);
-		});
+		expect(snakePosition[0].x2 - snakePosition[0].x1).toBe(
+			previousSnakePosition[0].x2 - previousSnakePosition[0].x1 + 3
+		);
+	});
 
-		it('draws food with its new position and clears the previous food', () => {
-			const ctx = (screen.getByTestId('canvas') as HTMLCanvasElement).getContext('2d');
-			const foodPosition = getFoodCurrentPosition();
-			const radius = get(GAME_PIECE_MIN_SIZE) / 2;
-
-			expect(ctx?.arc).toBeCalledWith(foodPosition.x, foodPosition.y, radius, 0, 2 * Math.PI);
-			expect(ctx?.fill).toBeCalledTimes(1);
-
-			expect(ctx?.clearRect).toBeCalledWith(
-				previousFoodPosition.x - radius,
-				previousFoodPosition.y - radius,
-				radius * 2,
-				radius * 2
-			);
-		});
-
-		it('increases the snake length', () => {
-			const snakePosition = getSnakeCurrentPosition();
-
-			expect(snakePosition[0].x2 - snakePosition[0].x1).toBe(
-				previousSnakePosition[0].x2 - previousSnakePosition[0].x1 + 3
-			);
-		});
-
-		it('updates the scoreboard', () => {
-			expect(scoreboard.textContent).toBe(`${level}`);
-		});
-
-		afterEach(() => {
-			SCOREBOARD.update(() => 0);
-			SNAKE_POSITION.update(() => [
-				{
-					x1: 130,
-					x2: 170,
-					y1: 97,
-					y2: 103,
-					direction: 'right'
-				}
-			]);
-			FOOD_POSITION.update(() => {
-				return { x: 50, y: 90 };
-			});
-		});
+	it('updates the scoreboard', () => {
+		expect(scoreboard.textContent).toBe(`${level}`);
 	});
 
 	afterEach(() => {
-		jest.runOnlyPendingTimers();
-		jest.useRealTimers();
+		SCOREBOARD.update(() => 0);
+		SNAKE_POSITION.update(() => [
+			{
+				x1: 130,
+				x2: 170,
+				y1: 97,
+				y2: 103,
+				direction: 'right'
+			}
+		]);
+		FOOD_POSITION.update(() => {
+			return { x: 50, y: 90 };
+		});
 	});
 });
