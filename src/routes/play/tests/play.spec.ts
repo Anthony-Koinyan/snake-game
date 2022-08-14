4340ca8937a7efb64bd7a3bbd2af5df2a3ce2a85
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-08-14 22:15:25 +0100

    test: Move food position setup to beforeEach

--- src/routes/play/tests/play.spec.ts
+++ src/routes/play/tests/play.spec.ts
@@ -25,15 +25,31 @@ afterAll(() => {
 });
 
 beforeEach(async () => {
+	FOOD_POSITION.update(() => {
+		return { x: 175, y: 99 };
+	});
+
 	render(Play);
 	await act();
 	await act();
 });
 
+afterEach(() => {
+	SCOREBOARD.update(() => 0);
+	FOOD_POSITION.update(() => ({ x: 50, y: 90 }));
+	SNAKE_POSITION.update(() => [
+		{
+			x1: 130,
+			x2: 170,
+			y1: 97,
+			y2: 103,
+			direction: 'right'
+		}
+	]);
+});
+
 describe('checking for default content on initial render', () => {
 	const [snakePosition] = get(SNAKE_POSITION);
-	const foodPosition = get(FOOD_POSITION);
-	const foodRadius = get(GAME_PIECE_MIN_SIZE) / 2;
 	let ctx: CanvasRenderingContext2D | null;
 
 	beforeEach(async () => {
@@ -51,6 +67,9 @@ describe('checking for default content on initial render', () => {
 	});
 
 	it('draws food', () => {
+		const foodPosition = get(FOOD_POSITION);
+		const foodRadius = get(GAME_PIECE_MIN_SIZE) / 2;
+
 		expect(ctx?.arc).toBeCalledWith(foodPosition.x, foodPosition.y, foodRadius, 0, 2 * Math.PI);
 		expect(ctx?.fill).toBeCalledTimes(1);
 	});
@@ -63,7 +82,7 @@ describe('checking for default content on initial render', () => {
 describe('snake can move', () => {
 	it('moves the snake', async () => {
 		const snakePosition = getSnakeCurrentPosition();
-		await advanceTimersByTime(16);
+		await advanceTimersByTime(16); // 1 animation frame
 		expect(getSnakeCurrentPosition()).not.toEqual(snakePosition);
 	});
 
@@ -72,22 +91,22 @@ describe('snake can move', () => {
 		expect(getSnakeCurrentPosition()[0].direction).toBe('right');
 
 		fireEvent.keyPress(window, { key: 'W', code: 'KeyW' });
-		await advanceTimersByTime(100);
+		await advanceTimersByTime(100); // 6 animation frames (16ms per frame)
 		expect(getSnakeCurrentPosition().length).toBe(2);
 		expect(getSnakeCurrentPosition()[0].direction).toBe('up');
 
 		fireEvent.keyPress(window, { key: 'D', code: 'KeyD' });
-		await advanceTimersByTime(100);
+		await advanceTimersByTime(100); // 6 animation frames (16ms per frame)
 		expect(getSnakeCurrentPosition().length).toBe(3);
 		expect(getSnakeCurrentPosition()[0].direction).toBe('right');
 
 		fireEvent.keyPress(window, { key: 'S', code: 'KeyS' });
-		await advanceTimersByTime(100);
+		await advanceTimersByTime(100); // 6 animation frames (16ms per frame)
 		expect(getSnakeCurrentPosition().length).toBe(4);
 		expect(getSnakeCurrentPosition()[0].direction).toBe('down');
 
 		fireEvent.keyPress(window, { key: 'A', code: 'KeyA' });
-		await advanceTimersByTime(100);
+		await advanceTimersByTime(100); // 6 animation frames (16ms per frame)
 		expect(getSnakeCurrentPosition().length).toBe(5);
 		expect(getSnakeCurrentPosition()[0].direction).toBe('left');
 	});
@@ -100,29 +119,25 @@ describe('game updates properly when snake eats food', () => {
 	let previousSnakePosition: SnakePosition[];
 
 	beforeEach(async () => {
-		FOOD_POSITION.update(() => {
-			return { x: 175, y: 99 };
-		});
-
 		previousFoodPosition = getFoodCurrentPosition();
 		previousSnakePosition = getSnakeCurrentPosition();
 
 		scoreboard = screen.getByText(0);
-		await advanceTimersByTime(64);
+		await advanceTimersByTime(64); // 4 animation frames (16ms per frame)
 	});
 
 	it('changes food position', () => {
 		expect(getFoodCurrentPosition()).not.toEqual(previousFoodPosition);
 	});
 
-	it('draws food with its new position and clears the previous food', () => {
+	it('draws food with its new position and clears the previous food', async () => {
 		const ctx = (screen.getByTestId('canvas') as HTMLCanvasElement).getContext('2d');
 		const foodPosition = getFoodCurrentPosition();
 		const radius = get(GAME_PIECE_MIN_SIZE) / 2;
 
+		await advanceTimersByTime(16); // 1 animation frame
 		expect(ctx?.arc).toBeCalledWith(foodPosition.x, foodPosition.y, radius, 0, 2 * Math.PI);
-		expect(ctx?.fill).toBeCalledTimes(1);
-
+		expect(ctx?.fill).toBeCalledTimes(2);
 		expect(ctx?.clearRect).toBeCalledWith(
 			previousFoodPosition.x - radius,
 			previousFoodPosition.y - radius,
@@ -141,21 +156,5 @@ describe('game updates properly when snake eats food', () => {
 
 	it('updates the scoreboard', () => {
 		expect(scoreboard.textContent).toBe(`${level}`);
-	});
-
-	afterEach(() => {
-		SCOREBOARD.update(() => 0);
-		SNAKE_POSITION.update(() => [
-			{
-				x1: 130,
-				x2: 170,
-				y1: 97,
-				y2: 103,
-				direction: 'right'
-			}
-		]);
-		FOOD_POSITION.update(() => {
-			return { x: 50, y: 90 };
-		});
 	});
 });
