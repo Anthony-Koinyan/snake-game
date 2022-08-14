5d266ee5465e8fab6e5325a028c89f9c233962fc
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-08-14 22:47:56 +0100

    feat: Increase snake length after it eats food

--- src/lib/Level.svelte
+++ src/lib/Level.svelte
@@ -5,9 +5,8 @@
 	import { DEFAULT_CANVAS_HEIGHT, DEFAULT_CANVAS_WIDTH } from './canvas/store';
 	import { SNAKE_POSITION } from './snake/store';
 	import { FOOD_POSITION } from './food/store';
-	import type { SnakePosition } from './snake';
 
-	$: snakeHead = $SNAKE_POSITION ? $SNAKE_POSITION[0] : null;
+	$: snakeHead = $SNAKE_POSITION[0];
 
 	$: eaten = (() => {
 		if (!snakeHead) return false;
@@ -47,6 +46,15 @@
 					x: Math.round(Math.random() * $DEFAULT_CANVAS_WIDTH),
 					y: Math.round(Math.random() * $DEFAULT_CANVAS_HEIGHT)
 				};
+			});
+
+			SNAKE_POSITION.update((position) => {
+				if (position[0].direction === 'right') position[0].x2 += 3;
+				if (position[0].direction === 'left') position[0].x1 += 3;
+				if (position[0].direction === 'up') position[0].y1 += 3;
+				if (position[0].direction === 'down') position[0].y2 += 3;
+
+				return position;
 			});
 		}
 	}
