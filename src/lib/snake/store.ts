d7e720e62988199634346ceaffb85ded37d12fa2
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-08-26 16:05:17 +0100

    refactor: Change difficulties store to an array
    This changes/adds:
    - the difficulties store to an array of difficulties instead
    of using a enum.
    
    - the difficulty store to a a string with
    the difficulty, instead of an object with a name and increment.
    
    - a check to see if icons have been loaded

--- src/lib/snake/store.ts
+++ src/lib/snake/store.ts
@@ -1,10 +1,13 @@
 // TODO: MOVE THIS TO GLOBAL STORE
 
 import { derived, writable } from 'svelte/store';
-import { DIFFICULTY } from '../stores';
+import { DIFFICULTIES, DIFFICULTY } from '../stores';
 import type { SnakePosition } from './index';
 
-export const SNAKE_SPEED = derived(DIFFICULTY, (difficulty) => difficulty.increment);
+export const SNAKE_SPEED = derived(
+	[DIFFICULTY, DIFFICULTIES],
+	([difficulty, difficulties]) => difficulties.indexOf(difficulty) + 1
+);
 export const SNAKE_POSITION = writable<SnakePosition[]>([
 	{
 		x1: 130,
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,26 +1,18 @@
 import { readable, writable } from 'svelte/store';
 
-/**FIXME: when difficulties is iterated on it return two copies of each enum
- */
-
-enum difficulties {
-	'baby steps' = 1,
-	'pretty easy',
-	'midly difficult',
-	'hard',
-	'nightmare',
-	'hell',
-	'beg for mercy',
-	'cry to your mommy'
-}
-
 // TODO: make this a store (I think, not sure🤷🏾‍♂️)
 export const RENDER_CONTEXT_KEY = Symbol();
-
+export const HAVE_ICONS_BEEN_LOADED = writable(false);
 export const GAME_PIECE_MIN_SIZE = readable(12);
 export const SCOREBOARD = writable(0);
-export const DIFFICULTIES = readable(difficulties);
-export const DIFFICULTY = writable({
-	name: 'baby steps',
-	increment: difficulties['baby steps']
-});
+export const DIFFICULTIES = readable([
+	'Baby Steps',
+	'Pretty Easy',
+	'Midly Difficult',
+	'Hard',
+	'Nightmare',
+	'Hell',
+	'Beg For Mercy',
+	'Cry To Your Mommy'
+]);
+export const DIFFICULTY = writable('Baby Steps');
