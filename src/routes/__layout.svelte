4e61e8d83959655cb75432462ac20a10abe39c87
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-08-26 18:30:45 +0100

    feat: Add button to redirect user to menu

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,35 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
+	import { afterNavigate } from '$app/navigation';
+	import { HAVE_ICONS_BEEN_LOADED } from '$lib/stores';
+
+	afterNavigate(() => {
+		if (!$HAVE_ICONS_BEEN_LOADED) {
+			const script = document.createElement('script');
+			script.setAttribute('type', 'module');
+			script.src = 'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js';
+			document.body.appendChild(script);
+
+			$HAVE_ICONS_BEEN_LOADED = true;
+		}
+	});
 </script>
 
-<section class="font-mono">
+{#if $page.url.pathname !== '/'}
+	<nav class="w-full p-5 border-y-black border-b-2 shadow-md">
+		<a
+			href="/"
+			class="flex text-3xl h-10 gap-4 ml-0 justify-start cursor-pointer w-fit hover:font-bold focus:font-bold focus:outline-none"
+		>
+			<ion-icon
+				name="chevron-back-outline"
+				class="text-3xl m-auto hover:font-bold focus:font-bold"
+			/>
+			<p class="my-auto">Go To Menu</p>
+		</a>
+	</nav>
+{/if}
+<main class="p-5 w-screen">
 	<slot />
-</section>
+</main>
