fc3d98e57108bd63297f65b6637fe2011663f9fd
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-08-26 16:08:05 +0100

    test: Test that the game difficulty can be changed

---
+++ src/routes/tests/settings.spec.ts
@@ -0,0 +1,38 @@
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/svelte';
+import { get } from 'svelte/store';
+import { DIFFICULTY } from '$lib/stores';
+import Settings from '../settings/index.svelte';
+
+describe('The difficulty can be changed', () => {
+	const getGameDifficulty = () => get(DIFFICULTY);
+
+	beforeEach(() => {
+		render(Settings);
+	});
+
+	it('renders the difficulty toggle', () => {
+		expect(screen.getByText('Set The Game Difficulty')).toBeInTheDocument();
+		expect(screen.getAllByRole('radio').length).toBe(8);
+	});
+
+	it('has "Baby Steps" as the default difficulty', () => {
+		expect(screen.getByRole('radio', { name: 'Baby Steps' })).toBeChecked();
+	});
+
+	it('changes the difficulty when a difficulty is selected', () => {
+		const [babySteps, prettyEasy, midlyDifficult] = screen.getAllByRole('radio');
+
+		fireEvent.click(midlyDifficult);
+		expect(midlyDifficult).toBeChecked();
+		expect(getGameDifficulty()).toBe('Midly Difficult');
+
+		fireEvent.click(prettyEasy);
+		expect(prettyEasy).toBeChecked();
+		expect(getGameDifficulty()).toBe('Pretty Easy');
+
+		fireEvent.click(babySteps);
+		expect(babySteps).toBeChecked();
+		expect(getGameDifficulty()).toBe('Baby Steps');
+	});
+});
