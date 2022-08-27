6244f57b875b19aa99b08538fb92721cbabd64d9
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-08-27 20:16:01 +0100

    style: Add translate-x animation to nav link

--- src/lib/NavLink.svelte
+++ src/lib/NavLink.svelte
@@ -5,7 +5,7 @@
 
 <a
 	href={url}
-	class="text-4xl sm:text-5xl inline-block my-auto p-4 hover:font-bold hover:border-4 hover:border-solid hover:border-black hover:shadow-md focus:font-bold focus:border-4 focus:border-solid focus:border-black focus:shadow-md focus:outline-none"
+	class="text-4xl sm:text-5xl inline-block my-auto p-4 hover:font-bold hover:border-4 hover:border-solid hover:border-black hover:shadow-md focus:font-bold focus:border-4 focus:border-solid focus:border-black focus:shadow-md focus:outline-none active:translate-x-3"
 	{target}
 >
 	<slot />
