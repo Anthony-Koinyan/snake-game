2fd1bb38f980fdff8e05af2ae5556f5f8d7b170c
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-09-02 03:41:34 +0100

    test: Check that canvas scales properly

---
+++ src/lib/tests/canvas.spec.ts
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/svelte';
+
+import Canvas from '../canvas/Canvas.svelte';
+
+test('the canvas scales properly', () => {
+	const target = document.createElement('div');
+
+	Object.defineProperty(target, 'clientWidth', {
+		writable: true,
+		configurable: true,
+		value: 950
+	});
+
+	Object.defineProperty(target, 'clientHeight', {
+		writable: true,
+		configurable: true,
+		value: 700
+	});
+
+	document.body.appendChild(target);
+
+	const initialCanvasDimensions = { width: 600, height: 400 };
+
+	const scaleFactor = Math.min(
+		target.clientHeight / initialCanvasDimensions.height,
+		target.clientWidth / initialCanvasDimensions.width
+	);
+
+	const pixelRatio = 2;
+	window.devicePixelRatio = pixelRatio;
+
+	const { getByTestId } = render(Canvas, { target, props: initialCanvasDimensions });
+	const canvas = getByTestId('canvas') as HTMLCanvasElement;
+
+	expect(canvas.width).toBe(Math.floor(initialCanvasDimensions.width * scaleFactor * pixelRatio));
+	expect(canvas.height).toBe(Math.floor(initialCanvasDimensions.height * scaleFactor * pixelRatio));
+	expect(canvas.style.width).toBe(`${Math.floor(initialCanvasDimensions.width * scaleFactor)}px`);
+	expect(canvas.style.height).toBe(`${Math.floor(initialCanvasDimensions.height * scaleFactor)}px`);
+	const ctx = canvas.getContext('2d');
+	expect(ctx?.setTransform).toBeCalledWith(1, 0, 0, 1, 0, 0);
+	expect(ctx?.scale).toBeCalledWith(scaleFactor * pixelRatio, scaleFactor * pixelRatio);
+});
