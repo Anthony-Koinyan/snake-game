913399d1e415dfa02901721f72a3126b94a39b1a
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-09-02 08:05:16 +0100

    test: Refactor tests to use storeIO functions

--- src/tests/play.spec.ts
+++ src/tests/play.spec.ts
@@ -1,27 +1,24 @@
 import '@testing-library/jest-dom';
 
-import { FOOD_POSITION } from '$lib/food/store';
-import { SNAKE_POSITION, SNAKE_SPEED } from '$lib/snake/store';
-import { GAME_PIECE_MIN_SIZE, SCOREBOARD } from '$lib/stores';
-import { get } from 'svelte/store';
 import { vi } from 'vitest';
 
 import { act, render, screen } from '@testing-library/svelte';
 
-import Play from '../routes/play/index.svelte';
-
-import type { SnakePosition } from '$lib/snake/types';
-import type { FoodPosition } from '$lib/food';
-
-const getSnakeCurrentPosition = () => get(SNAKE_POSITION);
-const getFoodCurrentPosition = () => get(FOOD_POSITION);
-const advanceTimersByTime = async (time: number) => {
-	await act(() => vi.advanceTimersByTime(time));
-};
+import Game from '../routes/play/index.svelte';
+import moveSnakeBySteps from './utils/moveSnakeBySteps';
+import {
+	getFoodPosition,
+	getGamePieceSize,
+	getScore,
+	getSnakePosition,
+	getSnakeSpeed,
+	setFoodPosition,
+	setScore,
+	setSnakePosition
+} from './utils/storeIO';
 
 beforeAll(() => {
 	vi.useFakeTimers();
-	vi.spyOn(window, 'requestAnimationFrame');
 });
 
 afterAll(() => {
@@ -29,38 +26,18 @@ afterAll(() => {
 	vi.useRealTimers();
 });
 
-beforeEach(async () => {
-	FOOD_POSITION.update(() => {
-		return { x: 175, y: 99 };
-	});
-
-	render(Play);
-});
-
-afterEach(() => {
-	SCOREBOARD.update(() => 0);
-	FOOD_POSITION.update(() => ({ x: 50, y: 90 }));
-	SNAKE_POSITION.update(() => [
-		{
-			x1: 130,
-			x2: 170,
-			y1: 97,
-			y2: 103,
-			direction: 'right'
-		}
-	]);
-});
-
 describe('checking for default content on initial render', () => {
+	let canvas: HTMLCanvasElement;
 	let ctx: CanvasRenderingContext2D | null;
 
-	beforeEach(async () => {
-		const canvas: HTMLCanvasElement = screen.getByTestId('canvas');
+	beforeEach(() => {
+		render(Game);
+		canvas = screen.getByTestId('canvas');
 		ctx = canvas.getContext('2d');
 	});
 
-	it('draws the snake', () => {
-		const [snakePosition] = get(SNAKE_POSITION);
+	test('draws the snake', () => {
+		const [snakePosition] = getSnakePosition();
 
 		expect(ctx?.fillRect).toBeCalledWith(
 			snakePosition.x1,
@@ -70,44 +47,56 @@ describe('checking for default content on initial render', () => {
 		);
 	});
 
-	it('draws food', () => {
-		const foodPosition = get(FOOD_POSITION);
-		const foodRadius = get(GAME_PIECE_MIN_SIZE) / 2;
+	test('draws food', () => {
+		const foodPosition = getFoodPosition();
+		const foodRadius = getGamePieceSize() / 2;
 
 		expect(ctx?.arc).toBeCalledWith(foodPosition.x, foodPosition.y, foodRadius, 0, 2 * Math.PI);
 		expect(ctx?.fill).toBeCalledTimes(1);
 	});
 
-	it('has the scoreboard', () => {
+	test('has the scoreboard', () => {
 		expect(screen.getByText(0)).toBeInTheDocument();
 	});
 });
 
 describe('game updates properly when snake eats food', () => {
-	let scoreboard: HTMLElement;
-	let previousFoodPosition: FoodPosition;
-	let previousSnakePosition: SnakePosition[];
-
-	beforeEach(async () => {
-		previousFoodPosition = getFoodCurrentPosition();
-		previousSnakePosition = getSnakeCurrentPosition();
-
-		scoreboard = screen.getByText(0);
-		await advanceTimersByTime(64); // 4 animation frames (16ms per frame)
+	beforeEach(() => {
+		setScore(0);
+		setFoodPosition({ x: 175, y: 99 });
+		setSnakePosition([{ x1: 130, x2: 170, y1: 97, y2: 103, direction: 'right' }]);
 	});
 
-	it('changes food position', () => {
-		expect(getFoodCurrentPosition()).not.toEqual(previousFoodPosition);
+	afterAll(() => {
+		setFoodPosition({ x: 50, y: 90 });
+		setSnakePosition([{ x1: 130, x2: 170, y1: 97, y2: 103, direction: 'right' }]);
 	});
 
-	it('draws food with its new position and clears the previous food', async () => {
+	test('changes food position', async () => {
+		const previousFoodPosition = getFoodPosition();
+
+		render(Game);
+		await act();
+		moveSnakeBySteps(4);
+		await act();
+
+		expect(getFoodPosition()).not.toEqual(previousFoodPosition);
+	});
+
+	test('draws food with its new position and clears the previous food', async () => {
+		const previousFoodPosition = getFoodPosition();
+
+		render(Game);
+		await act();
+		moveSnakeBySteps(4);
+		await act();
+
 		const ctx = (screen.getByTestId('canvas') as HTMLCanvasElement).getContext('2d');
-		const foodPosition = getFoodCurrentPosition();
-		const radius = get(GAME_PIECE_MIN_SIZE) / 2;
+		const foodPosition = getFoodPosition();
+		const radius = getGamePieceSize() / 2;
 
-		await advanceTimersByTime(16); // 1 animation frame
 		expect(ctx?.arc).toBeCalledWith(foodPosition.x, foodPosition.y, radius, 0, 2 * Math.PI);
-		expect(ctx?.clearRect).toBeCalledWith(
+		expect(ctx?.clearRect).toHaveBeenCalledWith(
 			previousFoodPosition.x - radius,
 			previousFoodPosition.y - radius,
 			radius * 2,
@@ -115,17 +104,29 @@ describe('game updates properly when snake eats food', () => {
 		);
 	});
 
-	it('increases the snake length', () => {
-		const snakePosition = getSnakeCurrentPosition();
+	test('increases the snake length', async () => {
+		const previousSnakePosition = getSnakePosition();
 
+		render(Game);
+		await act();
+		moveSnakeBySteps(4);
+		await act();
+
+		const snakePosition = getSnakePosition();
 		expect(snakePosition[0].x2 - snakePosition[0].x1).toBe(
 			previousSnakePosition[0].x2 - previousSnakePosition[0].x1 + 3
 		);
 	});
 
-	// TODO: this test expectation should be scoreboard + speed
-	it('updates the scoreboard', () => {
-		const speed = get(SNAKE_SPEED);
-		expect(scoreboard.textContent).toBe(`${speed}`);
+	test('updates the scoreboard', async () => {
+		const previousScore = getScore();
+		const { getByText } = render(Game);
+		const scoreboard = getByText(0);
+
+		await act();
+		moveSnakeBySteps(4);
+		await act();
+
+		expect(scoreboard.textContent).toBe(`${getSnakeSpeed() + previousScore}`);
 	});
 });
--- src/tests/utils/storeIO.ts
+++ src/tests/utils/storeIO.ts
@@ -1,17 +1,24 @@
 import { DEFAULT_CANVAS_HEIGHT, DEFAULT_CANVAS_WIDTH } from '$lib/canvas/store';
-import { SNAKE_POSITION } from '$lib/snake/store';
-import { DIFFICULTIES, DIFFICULTY, GAME_PIECE_MIN_SIZE } from '$lib/stores';
+import { FOOD_POSITION } from '$lib/food/store';
+import { SNAKE_POSITION, SNAKE_SPEED } from '$lib/snake/store';
+import { DIFFICULTIES, DIFFICULTY, GAME_PIECE_MIN_SIZE, SCOREBOARD } from '$lib/stores';
 import { get } from 'svelte/store';
 
 import type { SnakePosition } from '$lib/snake/types';
+import type { FoodPosition } from '$lib/food';
 
 const difficulties = get(DIFFICULTIES);
 export type Difficulty = typeof difficulties[number];
-export const setSnakePosition = (newPosition: SnakePosition[]) => SNAKE_POSITION.set(newPosition);
+export const getScore = () => get(SCOREBOARD);
+export const setScore = (score: number) => SCOREBOARD.set(score);
+export const getSnakeSpeed = () => get(SNAKE_SPEED);
 export const getGameDifficulty = () => get(DIFFICULTY);
 export const setGameDifficulty = (difficulty: Difficulty) => DIFFICULTY.set(difficulty);
 export const getGamePieceSize = () => get(GAME_PIECE_MIN_SIZE);
 export const setGamePieceSize = (size: number) => GAME_PIECE_MIN_SIZE.set(size);
+export const setFoodPosition = (newPosition: FoodPosition) => FOOD_POSITION.set(newPosition);
+export const getFoodPosition = () => get(FOOD_POSITION);
+export const setSnakePosition = (newPosition: SnakePosition[]) => SNAKE_POSITION.set(newPosition);
 export const getSnakePosition = () =>
 	JSON.parse(JSON.stringify(get(SNAKE_POSITION))) as SnakePosition[];
 export const getCanvasBoundaryDimensions = () => {
