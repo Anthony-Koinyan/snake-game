44bad22b021c3018140cd3c106a74b5e75bcc9e7
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-09-06 23:28:54 +0100

    svelte-migrate: renamed files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,18 +7,20 @@
 	<nav class="w-full p-5 border-y-black border-b-2 shadow-md">
 		<a
 			href="/"
-			class="flex text-3xl h-10 gap-4 ml-0 justify-start cursor-pointer w-fit hover:font-bold focus:font-bold focus:outline-none stroke-[2.5] hover:stroke-[3] focus:stroke-[3] active:-translate-x-4"
+			class="flex text-3xl h-10 gap-4 ml-0 justify-start cursor-pointer w-fit group active:-translate-x-4"
 		>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
 				viewBox="0 0 24 24"
 				stroke="currentColor"
-				class="w-7 h-7 m-auto"
+				class="w-7 h-7 m-auto stroke-[2.5] group-hover:stroke-[3] group-focus:stroke-[3]"
 			>
 				<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
 			</svg>
-			<p class="my-auto">Go To Menu</p>
+			<p class="my-auto group-hover:font-bold group-focus:font-bold group-focus:outline-none">
+				Go To Menu
+			</p>
 		</a>
 	</nav>
 {/if}
