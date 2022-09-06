0bf0cb199eee8610d34a5eb834216d53a4cd11c7
Anthony-Koinyan <aaa.koinyan@gmail.com>
2022-09-07 00:39:10 +0100

    refactor: Move all the markup for the main menu
    Before the was a NavLink component that had all the markup and styling
    for the main menu. It didn't make much sense to have there, so I moved
    it to +page.svelte in the root of the routes directory

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,39 @@
 <script lang="ts">
-	import NavLink from '$lib/NavLink.svelte';
+	const links = [
+		{
+			url: '/play#continue',
+			text: 'Continue'
+		},
+		{
+			url: '/play#new-game',
+			text: 'New Game'
+		},
+		{
+			url: '/settings',
+			text: 'Settings'
+		},
+		{
+			url: '/high-scores',
+			text: 'High Scores'
+		},
+		{
+			url: 'https://github.com/Anthony-Koinyan/snake-game',
+			text: 'Repo'
+		}
+	];
 </script>
 
 <div class="flex flex-col w-full h-[calc(100vh-2.5rem)]">
 	<nav class="grid gap-3 lg:gap-5 w-fit h-2/3 lg:h-3/4 align-baseline m-auto">
-		<NavLink url="/play">Continue</NavLink>
-		<NavLink url="/play">New Game</NavLink>
-		<NavLink url="/settings">Settings</NavLink>
-		<NavLink url="/high-scores">High Scores</NavLink>
-		<NavLink url="https://github.com/Anthony-Koinyan/snake-game" target="_blank">Repo</NavLink>
+		{#each links as link}
+			<a
+				href={link.url}
+				class="text-4xl sm:text-5xl inline-block my-auto p-4 hover:font-bold hover:border-4 hover:border-solid hover:border-black hover:shadow-md focus:font-bold focus:border-4 focus:border-solid focus:border-black focus:shadow-md focus:outline-none active:translate-x-3"
+				target={link.url.charAt(0) === '/' ? null : '_blank'}
+				rel={link.url.charAt(0) === '/' ? null : 'noopener'}
+			>
+				{link.text}
+			</a>
+		{/each}
 	</nav>
 </div>
